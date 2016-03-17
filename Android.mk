c3ae3407fc4490b0537ad376052dba82733f3cf7
dianlujitao <dianlujitao@gmail.com>
2016-03-18 00:51:43 +0800

    Fix derp
    Change-Id: Ia492756038979b339262d0ee029d2d05f1eccdb4

--- Android.mk
+++ Android.mk
@@ -5,6 +5,8 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
+ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
